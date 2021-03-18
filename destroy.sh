2c70869265d56a2e2ef00bd096fdafdd3f79e93d
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-03-18 08:11:17 +0100

    fix: the destroy script

--- destroy.sh
+++ destroy.sh
@@ -3,16 +3,16 @@
 source ./.env
 source ./util.sh
 
-# change_set_name=$stack_name-change-set-$(date +%s)
-
 if ! stack_exists $STACK_NAME; then exit 0; fi
 
 read -n 1 -p "destroy the $STACK_NAME stack - are you sure? (y/n) " answer
 echo
 
-echo "destroyin the $STACK_NAME stack"
-
 if [[ "${answer,,}" != "y" ]]; then exit 0; fi
+
+echo "destroyin the $STACK_NAME stack"
 
 aws cloudformation delete-stack --stack-name $STACK_NAME
 #   --retain-resources
+
+aws cloudformation wait stack-delete-complete --stack-name $STACK_NAME
