43912bc2ede699072b98353d379a0c58daf800f5
chiefbiiko <noah.anabiik.schwarz@gmail.com>
2021-03-25 12:40:07 +0100

    upd: ssh 4 bluford

--- ssh.sh
+++ ssh.sh
@@ -5,7 +5,7 @@ source ./.env
 stack="$(aws cloudformation describe-stacks --stack-name $STACK_NAME)"
 
 public_ip="$( \
-  jq -r '.Stacks[] | select(.StackName == "bluford") | .Outputs[] | select(.OutputKey == "PublicIp") | .OutputValue' <<< "$stack" \
+  jq -r ".Stacks[] | select(.StackName == \"$STACK_NAME\") | .Outputs[] | select(.OutputKey == \"PublicIp\") | .OutputValue" <<< "$stack" \
 )"
 
 ssh -i $HOME/.ssh/$SSH_PRIVATE_KEY_NAME $EC2_USERNAME@$public_ip
